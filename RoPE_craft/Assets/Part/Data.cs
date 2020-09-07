ffc4323b65d67df4f03e759195361868ea9cb0a4
tayutayufk <takayasu1j202h19@gmail.com>
2020-09-07 21:57:19 +0900

    できてる？

---
+++ RoPE_craft/Assets/Part/Data.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Data : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Vector3 size;
+    public float Mass;
+
+    public bool coll = false;
+    void OnCollisionEnter(Collision collision) {
+        coll = true;
+    }
+	 
+
+    void OnCollisionExit(Collision collision) {
+	    coll = false;
+    }
+	 
+    void OnCollisionStay(Collision collision) {
+	    coll = true;
+    }
+}
