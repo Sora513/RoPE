fb449fcc9e3b72d0fecfde67083fa22a62231644
tayutayufk <takayasu1j202h19@gmail.com>
2020-09-10 23:28:21 +0900

    メニュー画面を作りました

---
+++ RoPE_craft/Assets/Script/Direcotr.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Direcotr : MonoBehaviour
+{
+    public GameObject select;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //select = GameObject.Find("BasicBlock");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.GetComponent<SmallWindow>().AddedBlock = select;
+        this.GetComponent<BlockAdder>().BlockPrefab = select;
+    }
+}
---
+++ RoPE_craft/Assets/Script/MenuMain.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Linq;
+
+public class MenuMain : MonoBehaviour
+{
+    // Start is called before the first frame update
+    GameObject robot;
+    void Start()
+    {
+
+        robot = GameObject.Find("Robot");
+        var files = new DirectoryInfo(Application.dataPath + "/RobotSave/")
+                        .GetFiles("*.json")
+                        .OrderBy(fi => fi.CreationTime)
+                        .Select(fi => fi.FullName)
+                        .ToArray();
+
+        
+        foreach(var f in files)
+        {
+            Debug.Log(f);
+        }
+
+
+        Load loadscript = GetComponent<Load>();
+        if(files != null)
+        {
+            loadscript.filename = files[files.Count()-1];
+            loadscript.LoadJson(true);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        robot.transform.Rotate(0f,0.3f,0f);   
+    }
+}
